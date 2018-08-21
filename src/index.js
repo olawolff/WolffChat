6fafbae712ab5948b83b8f80809d2b3a3cabe875
Lucas Vinco <lucasvinco@hotmail.com>
2018-08-21 12:18:54 -0300

    melhorias no processo de build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index.init.js';
+//import './components/css/chat.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
